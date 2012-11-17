0122bb619018612f107a6a106233868dc61238fc
tetigi <luke.tomlin@gmail.com>
2012-11-17 14:37:29 +0000

    Got helicopter IR to work, with commands. Adding Haskell front-end now.

--- src/Arduino/majom/majom.ino
+++ src/Arduino/majom/majom.ino
@@ -1,70 +1,85 @@
 //Arduino code to control a helicotper.
-int IRledPin =  12;    
-int incomingByte = 0;
+int IRledPin =  8; //currently hard-coded for speed. Need to change.
+int pulseOneOn = 320;
+int pulseOneOff = 680;
+int pulseZeroOn = 320;
+int pulseZeroOff = 280;
+int headerOn = 2000;
+int headerOff = 2000;
+int footerOn = 300;
+int messageLength = 120000;
+
 int pulseValues[32];
 int pulseLength = 0;
 
 
 void setup() {                
-  // initialize the IR digital pin as an output:
   pinMode(IRledPin, OUTPUT); 
-  Serial.begin(9600);
-  
-  for (int i=0; i < 13; i++) 
-    pulseValues[i] = 0;
-  //yaw
-  pulseValues[0] = 0;
-  pulseValues[1] = 0;
-  pulseValues[2] = 1;
-  pulseValues[3] = 1;
-  pulseValues[4] = 1;
-  pulseValues[5] = 1;
-  pulseValues[6] = 1;
-  pulseValues[7] = 1;
-  //pitch
-  pulseValues[8] = 0;
-  pulseValues[9] = 0;
-  pulseValues[10] = 1;
-  pulseValues[11] = 1;
-  pulseValues[12] = 1;
-  pulseValues[13] = 1;
-  pulseValues[14] = 1;
-  pulseValues[15] = 1;
-  //throttle
-  pulseValues[16] = 0;
-  pulseValues[17] = 0;
-  pulseValues[18] = 1;
-  pulseValues[19] = 1;
-  pulseValues[20] = 1;
-  pulseValues[21] = 1;
-  pulseValues[22] = 1;
-  pulseValues[23] = 1;
-  //correction
-  pulseValues[24] = 0;
-  pulseValues[25] = 0;
-  pulseValues[26] = 1;
-  pulseValues[27] = 1;
-  pulseValues[28] = 1;
-  pulseValues[29] = 1;
-  pulseValues[30] = 1;
-  pulseValues[31] = 1;
+  setYaw(63);
+  setPitch(100);
+  setThrottle(0, 1);
+  setCorrection(63);
   
 }
 
+void setBitRange(int start, int fin, int val) {
+  for(int i=fin-1; i>=start; i--) {
+    pulseValues[i] = val && B1;
+    val = (val >> 1);
+  }
+}
+
+void setYaw(int val) {
+  val = clamp(0, 127, val);
+  setBitRange(0,8,val);
+}
+
+void setPitch(int val) {
+  val = clamp(0, 127, val);
+  setBitRange(8,16,val);
+}
+
+void setThrottle(int val, int channel) {
+  val = clamp(0, 127, val);
+  if(channel == 2) {
+    setBitRange(16,24,val+128);
+  } else {
+    setBitRange(16,24,val);
+  }
+}
+
+void setCorrection(int val) {
+  val = clamp(0, 127, val);
+  setBitRange(24,32,val);
+}
+
+int clamp(int a, int b, int x) {
+  if(x < a) {
+    return a;
+  }else if(x > b) {
+     return b;
+  }
+  return x;
+}
+  
 void loop() {
       SendCode();  
+      delay(25);
 }
 
 void pulseIR(long microsecs) {
   cli();  // this turns off any background interrupts
 
   while (microsecs > 0) {
-    // 38 kHz is about 13 microseconds high and 13 microseconds low
-    digitalWrite(IRledPin, HIGH);  // this takes about 3 microseconds to happen
-    delayMicroseconds(10);         // hang out for 10 microseconds
-    digitalWrite(IRledPin, LOW);   // this also takes about 3 microseconds
-    delayMicroseconds(10);         // hang out for 10 microseconds
-
+    //IR
+    PORTB |= (1<<0);
+    delayMicroseconds(13);
+    PORTB &= ~(1<<0);
+    PORTB &= ~(1<<0);
+    PORTB &= ~(1<<0);
+    PORTB &= ~(1<<0);
+    PORTB &= ~(1<<0);
+    delayMicroseconds(12);
     // so 26 microseconds altogether
     microsecs -= 26;
 
@@ -75,16 +90,16 @@ void pulseIR(long microsecs) {
 
 void Zero()
 {  
-  pulseIR(320);
-  delayMicroseconds(280);
-  pulseLength += 600;
+  pulseIR(pulseZeroOn);
+  delayMicroseconds(pulseZeroOff);
+  pulseLength += (pulseZeroOn + pulseZeroOff);
 }
 
 void One()
 {
-  pulseIR(320);
-  delayMicroseconds(680); 
-  pulseLength += 1000;
+  pulseIR(pulseOneOn);
+  delayMicroseconds(pulseOneOff); 
+  pulseLength += (pulseOneOn + pulseOneOff);
 }
 
 void sendPulseValue(int pulseValue)
@@ -97,48 +112,15 @@ void sendPulseValue(int pulseValue)
 
 void SendCode() {
 
-  while (true)
-  {
+  pulseIR(headerOn);
+    delayMicroseconds(headerOff);
+    pulseLength= headerOn + headerOff;
 
-    pulseIR(2000);
-    delayMicroseconds(2000);
-    pulseLength=4000;
-
-    sendPulseValue(pulseValues[0]);
-    sendPulseValue(pulseValues[1]);
-    sendPulseValue(pulseValues[2]);
-    sendPulseValue(pulseValues[3]);
-    sendPulseValue(pulseValues[4]);
-    sendPulseValue(pulseValues[5]);
-    sendPulseValue(pulseValues[6]);
-    sendPulseValue(pulseValues[7]);
-    sendPulseValue(pulseValues[8]);
-    sendPulseValue(pulseValues[9]);
-    sendPulseValue(pulseValues[10]);
-    sendPulseValue(pulseValues[11]);
-    sendPulseValue(pulseValues[12]);
-    sendPulseValue(pulseValues[13]);
-    sendPulseValue(pulseValues[14]);
-    sendPulseValue(pulseValues[15]);
-    sendPulseValue(pulseValues[16]);
-    sendPulseValue(pulseValues[17]);
-    sendPulseValue(pulseValues[18]);
-    sendPulseValue(pulseValues[19]);
-    sendPulseValue(pulseValues[20]);
-    sendPulseValue(pulseValues[21]);
-    sendPulseValue(pulseValues[22]);
-    sendPulseValue(pulseValues[23]);
-    sendPulseValue(pulseValues[24]);
-    sendPulseValue(pulseValues[25]);
-    sendPulseValue(pulseValues[26]);
-    sendPulseValue(pulseValues[27]);
-    sendPulseValue(pulseValues[28]);
-    sendPulseValue(pulseValues[29]);
-    sendPulseValue(pulseValues[30]);
-    sendPulseValue(pulseValues[31]);
+    for(int i=0; i < 32; i++) {
+      sendPulseValue(pulseValues[i]);
+    }
 
     //Footer
-    pulseIR(320); 
-    delayMicroseconds( (28640 - pulseLength) ); 
-   } 
+    pulseIR(footerOn);
+    delayMicroseconds(messageLength - pulseLength - footerOn); 
 }
