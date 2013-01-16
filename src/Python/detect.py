9d4079bc1624be00f8c329815e864c587332ebf6
tetigi <luke.tomlin@gmail.com>
2013-01-16 17:07:42 +0000

    Playing around with simplecv

--- src/Python/detect.py
+++ src/Python/detect.py
@@ -1,13 +1,20 @@
 from SimpleCV import *
 cam = Kinect()
 
-while True:
-  img = cam.getImage()
-  dist = img.colorDistance(Color.BLACK).dilate(2)
-  segmented = dist.stretch(200,255)
-  blobs = segmented.findBlobs()
-  if blobs:
-    circles = blobs.filter([b.isCircle(0.2) for b in blobs])
-    if circles:
-      img.drawCircle((circles[-1].x, circles[-1].y),circles[-1].radius(),Color.BLUE,3)
-  img.show()
+def basic(img):
+    dist = img.colorDistance(Color.WHITE).dilate(2)
+    segmented = dist.stretch(180,255)
+    blobs = segmented.findBlobs()
+    return blobs
+
+def detect(foo):
+  while True:
+    img = cam.getImage().crop(90,0,420,480)
+    ret = foo(img)
+    ret.show()
+  #if blobs:
+  #  circles = blobs.filter([b.isCircle(0.2) for b in blobs])
+  #  if circles:
+  #    img.drawCircle((circles[-1].x, circles[-1].y),circles[-1].radius(),Color.BLUE,3)
+  #img.show()
+
