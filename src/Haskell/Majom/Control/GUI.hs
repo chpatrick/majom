a36b8d7541317122e6235483c5d9d40736dfb9d5
tetigi <luke.tomlin@gmail.com>
2013-05-22 22:29:22 +0100

    Added some controls to the GUI. Added more to the report.

--- src/Haskell/Majom/Control/GUI.hs
+++ src/Haskell/Majom/Control/GUI.hs
@@ -48,19 +48,19 @@ guiSetup flyer = do
   lab1 <- Gtk.labelNew (Just "Enter KP")
   Gtk.boxPackStart hb1 lab1 Gtk.PackNatural 0
   nrfield1 <- Gtk.entryNew
-  Gtk.entrySetText nrfield1 "0"
+  Gtk.entrySetText nrfield1 "5"
   Gtk.boxPackStart hb1 nrfield1 Gtk.PackNatural 5
 
   lab1 <- Gtk.labelNew (Just "Enter KI")
   Gtk.boxPackStart hb1 lab1 Gtk.PackNatural 0
   nrfield2 <- Gtk.entryNew
-  Gtk.entrySetText nrfield2 "0"
+  Gtk.entrySetText nrfield2 "0.1"
   Gtk.boxPackStart hb1 nrfield2 Gtk.PackNatural 5
 
   lab1 <- Gtk.labelNew (Just "Enter KD")
   Gtk.boxPackStart hb1 lab1 Gtk.PackNatural 0
   nrfield3 <- Gtk.entryNew
-  Gtk.entrySetText nrfield3 "0"
+  Gtk.entrySetText nrfield3 "5"
   Gtk.boxPackStart hb1 nrfield3 Gtk.PackNatural 5
 
   Gtk.onEntryActivate nrfield1 (updateKP flyer nrfield1)
@@ -122,6 +122,12 @@ interpretKeyPress flyer valMap = do
     "Right" -> do -- Yaw right
       liftIO $ chValue "Right" Yaw (-10)
       return True
+    "g" -> do -- Throttle down
+      liftIO $ chValue "g" Throttle (-1)
+      return True
+    "h" -> do -- Throttle up
+      liftIO $ chValue "h" Throttle (1)
+      return True
     "d" -> do -- Throttle down
       liftIO $ chValue "d" Throttle (-10)
       return True
@@ -162,6 +168,7 @@ interpretKeyPress flyer valMap = do
       chValue :: String -> Option -> Int -> IO ()
       chValue key o v = do
         --putStrLn key
+        putStrLn $ "Setting " ++ (show o) ++ " to " ++ (show v)
         value <- readIORef $ valMap Map.! o
         setFly flyer o (value + v)
         writeIORef (valMap Map.! o) (value + v)
